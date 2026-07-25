72fbf9e4d1808361cdbe58173e975313438b4e09
nvshibu <nvshibuse@gmail.com>
2026-07-25 20:26:27 +0530

    first question

---
+++ launchpad.java
@@ -0,0 +1,27 @@
+class launchpad
+{
+  /* Method Implementation to calculate the sum of two integers */
+  private static int total(int data1, int data2)
+  {
+    return data1 + data2;
+  }
+
+  /* Method Implementation to find the largest of two integers */
+
+  //write your code here
+
+
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World!");
+    
+    /* A method to calculate the sum of two integers */
+    int sum = total(5, 10);
+    System.out.println("Total: " + sum);
+
+    /* A method to print the largest of two integers */
+    
+    //write your code here
+  }
+}
